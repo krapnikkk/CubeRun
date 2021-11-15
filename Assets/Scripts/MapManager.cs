3451916570cd99020928818bcef9a260bbcacce4
krapnik <krapnik@qq.com>
2021-11-15 22:45:29 +0800

    update map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     private GameObject m_prefab_tile;
+    private GameObject m_prefab_wall;
     private Transform m_Transform;
+    private Color colorOne = new Color(124 / 255f, 155 / 255f, 230 / 255f);
+    private Color colorTwo = new Color(125 / 255f, 169 / 255f, 233 / 255f);
     // Start is called before the first frame update
     void Start()
     {
         m_prefab_tile = Resources.Load("tile_white") as GameObject;
+        m_prefab_wall = Resources.Load("wall") as GameObject;
         m_Transform = gameObject.GetComponent<Transform>();
         createMapItem();
 
@@ -17,15 +21,39 @@ public class MapManager : MonoBehaviour
 
     private void createMapItem()
     {
-        float span = Mathf.Sqrt(2)*0.254f;
+        float span = Mathf.Sqrt(2) * 0.254f;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Vector3 pos = new Vector3(j * span, 0, i * span);
+                Vector3 rot = new Vector3(-90, 45, 0);
+                GameObject obj = null;
+                if (j == 0 || j == 5)
+                {
+                    obj = GameObject.Instantiate(m_prefab_wall, pos, Quaternion.Euler(rot));
+                }
+                else
+                {
+                    obj = GameObject.Instantiate(m_prefab_tile, pos, Quaternion.Euler(rot));
+                    obj.GetComponent<Transform>().Find("normal_a2").GetComponent<MeshRenderer>().material.color = colorOne;
+
+                }
+                obj.GetComponent<Transform>().SetParent(m_Transform);
+                obj.GetComponent<MeshRenderer>().material.color = colorOne;
+            }
+        }
+
         for (int i = 0; i < 10; i++)
         {
             for (int j = 0; j < 5; j++)
             {
-                Vector3 pos = new Vector3(j * span, 0, i * span);
+                Vector3 pos = new Vector3(j * span + span / 2, 0, i * span + span / 2);
                 Vector3 rot = new Vector3(-90, 45, 0);
                 GameObject tile = GameObject.Instantiate(m_prefab_tile, pos, Quaternion.Euler(rot));
                 tile.GetComponent<Transform>().SetParent(m_Transform);
+                tile.GetComponent<Transform>().Find("normal_a2").GetComponent<MeshRenderer>().material.color = colorTwo;
+                tile.GetComponent<MeshRenderer>().material.color = colorTwo;
             }
         }
     }
